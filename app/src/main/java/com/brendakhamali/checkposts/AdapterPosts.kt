51415daae85de0f351a024fd559f2a30710fb816
kimura-khamali <kimurakhamali@gmail.com>
2024-08-01 01:22:29 +0300

    commit

---
+++ app/src/main/java/com/brendakhamali/checkposts/AdapterPosts.kt
@@ -0,0 +1,33 @@
+package com.brendakhamali.checkposts
+
+//class AdapterPosts {
+//}
+
+class AdapterPost(val body: List<Post>, val context:Context):RecyclerView.Adapter<PostViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val binding=PostListItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return PostViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return body.size
+    }
+
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        val body=body[position]
+        holder.binding.tittle.text=body.tittle
+        holder.binding.id.text=body.id.toString()
+        holder.binding.user.text=body.userId.toString()
+        holder.binding.body.text=body.body
+        holder.binding.clPost.setOnClickListener{
+            val intent = Intent(context,CommentsActivity::class.java)
+            intent.putExtra("POST_ID", body.id)
+            context.startActivity(intent)
+        }
+    }
+}
+
+class PostViewHolder(var binding: PostListItemBinding):RecyclerView.ViewHolder(binding.root){
+
+}
